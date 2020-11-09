951e7b49a66ed775351820af2a111dda61d33584
kmnkit <tokyobuy2020@gmail.com>
2020-11-09 11:11:00 +0900

    settings modify

--- config/settings.py
+++ config/settings.py
@@ -88,14 +88,24 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if DEBUG:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
     }
-}
-
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "HOST": "airbnb-clone.cdo3vcyxnmpo.ap-northeast-1.rds.amazonaws.com",
+            "NAME": "airbnb-clone",
+            "USER": "postgresql",
+            "PASSWORD": "123nomad123nomad",
+            "PORT": "5432",
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
